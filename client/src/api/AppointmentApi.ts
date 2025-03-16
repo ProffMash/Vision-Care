f30fa06167a5a47e930ce7e5bca6437ef4ffcfa7
david <kdavidmacharia8485@gmail.com>
2025-03-16 16:27:20 +0300

    render api

--- client/src/api/AppointmentApi.ts
+++ client/src/api/AppointmentApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/appointments/';
+const API_URL = 'https://dorapi.onrender.com/api/appointments/';
 
 export interface Appointment {
     id?: number;
--- client/src/api/contactApi.ts
+++ client/src/api/contactApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/contacts/';
+const API_URL = 'https://dorapi.onrender.com/api/contacts/';
 
 export interface Contact {
     contact_id?: number;
